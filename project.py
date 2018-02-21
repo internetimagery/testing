1195c1e733690318078ce24f23d8142c3e2b6775
Jason Dixon <jason.dixon.email@gmail.com>
2018-02-21 23:31:49 +1300

    different layout examples

--- project.py
+++ project.py
@@ -10,8 +10,15 @@ class Project(config.Config):
         config.Config.__init__(s, os.path.expanduser("~/project_config.json"))
         s.load()
 
+    def new(s):
+        """ Set up new project information """
+        s.put("studio", "name", "pixar")
+        return s
+
 Project = Project() # Single shared project class
 
 if __name__ == '__main__':
     ws = os.path.join(os.path.dirname(__file__), "test_workspace.json")
-    Project.set_path(ws).load().put("show", "mll", "/path/to/config").save()
+    Project.set_path(ws).new().put("show", "mll", "/path/to/config")
+    Project.put("show", "aba", {"010/0010": "/path/to/scene/config.json"})
+    Project.put("shot", "010/0010", "/path/to/shot/config.json").save()
