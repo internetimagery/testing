750a673b1f8b0b0d0768ec7678abe3a9ce9833a6
Jason Dixon <jason.dixon.email@gmail.com>
2018-02-22 07:07:31 +1300

    path helper

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 # Base level workspace functionality.
 
 from __future__ import print_function
+import os.path
 import json
 
 class Config(object):
 
     def __init__(s, path):
         """ Load and use config file """
-        s._config_path = path
+        s.set_config_path(path)
         s._data = {}
 
     def load(s):
@@ -26,35 +27,45 @@ class Config(object):
         return s.load()
 
     def save(s):
-        """ Save data to the workspace """
+        """ Save data to the config """
         with open(s._config_path, "w") as f:
             json.dump(s._data, f, indent=4)
         return s
 
-    def set_path(s, path):
-        """ Change location of workspace file. """
+    def set_config_path(s, path):
+        """ Change location of config file. """
         s._config_path = path
+        s._config_path_dir = os.path.dirname(path)
         return s
 
-    def put(s, category, key, value):
-        """ Add some data to workspace """
-        s._data[category] = s._data.get(category, {})
-        s._data[category][key] = value
+    def set(s, group, attribute, value):
+        """ Add some data to config """
+        s._data[group] = s._data.get(group, {})
+        s._data[group][attribute] = value
         return s
 
-    def get(s, category, key, default=None):
-        """ Get some data from workspace """
-        cat = s._data.get(category, {})
-        return cat.get(key, cat.get("default", default))
+    def get(s, group, attribute, default=None):
+        """ Get some data from config """
+        cat = s._data.get(group, {})
+        return cat.get(attribute, cat.get("default", default))
+
+    def set_path(s, group, attribute, path):
+        """ Add a relative path to config """
+        return s.set(group, attribute, os.path.relpath(path, s._config_path_dir).replace(os.path.sep, "/"))
+
+    def get_path(s, group, attribute):
+        """ Get relative path from config """
+        return os.path.realpath(os.path.join(s._config_path_dir, s.get(group, attribute, "")))
 
 if __name__ == '__main__':
-    import os.path
     test = os.path.join(os.path.dirname(__file__), "config_test.json")
     conf = Config(test)
-    conf.put("category1", "key", "value")
+    conf.set("category1", "key", "value")
     assert "value" == conf.get("category1", "key")
     assert "value2" == conf.get("category1", "key2", "value2")
-    conf.put("category1", "default", "val-you")
+    conf.set("category1", "default", "val-you")
     assert "val-you" == conf.get("category1", "missing")
     assert "val-you" == conf.get("category1", "missingtoo", "val-all")
+    conf.set_path("path_test", "path", "file/over.here")
+    assert os.path.realpath(os.path.join(os.path.dirname(test), "file/over.here")) == conf.get_path("path_test", "path")
     conf.save()
