be60bfdceb4c1142e293bc5ba533a1e544d4a796
Jason Dixon <jdixon@wetafx.co.nz>
2018-02-27 14:55:53 +1300

    more stuff

--- asset.py
+++ asset.py
@@ -3,28 +3,27 @@ from __future__ import print_function
 import os.path
 import tinydb
 
-
-
-class Objects(tinydb.database.Table):
+class Factory(tinydb.database.Table):
     def _load_type(s, doc):
         elem = TYPES.get(doc.get("_type"), Unknown)(s)
         elem._load_doc(doc)
         return elem
     def get(s, *args, **kwargs):
-        res = tinydb.database.Table.get(s, *args, **kwargs)
+        res = super(Factory, s).get(*args, **kwargs)
+        print(res)
+        print(tinydb.database.Table.get(s, *args, **kwargs))
         return res and s._load_type(res)
     def search(s, *args, **kwargs):
-        return [s._load_type(a) for a in tinydb.database.Table.search(s, *args, **kwargs)]
+        return [s._load_type(a) for a in super(Factory, s).search(*args, **kwargs)]
     def all(s):
-        return [s._load_type(a) for a in tinydb.database.Table.all(s)]
+        return [s._load_type(a) for a in super(Factory, s).all()]
     def new_asset(s, name):
-        asset = Asset(s)
-        asset.name = name
+        asset = Asset(s, name)
         return asset
 
 class Entity(object):
-    def __init__(s, type, db):
-        s._doc = {"_type":type, "_name":""}
+    def __init__(s, type, db, name):
+        s._doc = {"_type":type, "_name":name}
         s._id = None
         s._db = db
     def _load_doc(s, doc):
@@ -36,7 +35,7 @@ class Entity(object):
             s._db.update(s._doc, doc_id=s._id)
     @property
     def type(s):
-        return s._type
+        return s._doc.get("_type")
     def name():
         def fget(s):
             return s._doc.get(["_name"], "")
@@ -46,12 +45,12 @@ class Entity(object):
     name = property(**name())
 
 class Unknown(Entity):
-    def __init__(s, db):
-        Entity.__init__(s, "unknown", db)
+    def __init__(s, db, name=""):
+        super(Unknown, s).__init__("unknown", db, name)
 
 class Asset(Entity):
-    def __init__(s, db):
-        Entity.__init__(s, "asset", db)
+    def __init__(s, db, name=""):
+        super(Asset, s).__init__("asset", db, name)
         s._doc["path"] = ""
     def path():
         def fget(s):
@@ -62,7 +61,7 @@ class Asset(Entity):
     path = property(**path())
 
 def DB(*args, **kwargs):
-    tinydb.TinyDB.table_class = Objects
+    tinydb.TinyDB.table_class = Factory
     return tinydb.TinyDB(*args, **kwargs)
 
 TYPES = {
@@ -72,12 +71,14 @@ TYPES = {
 ################################
 
 ROOT = os.path.join(os.path.dirname(__file__), "DB_Test.db")
+if os.path.exists(ROOT):
+    os.unlink(ROOT)
 
 db = DB(ROOT, indent=4)
 asset = db.new_asset("my asset")
-asset.name = "Asset this!"
 asset.path = "Over/there/file"
 asset.save()
-res = db.get(tinydb.where("_name") == "my asset")
-print(res)
-# print( type(res))
+q = tinydb.Query()
+print(db.all())
+# res = db.get(q.path == "Over/there/file")
+# print(res)
