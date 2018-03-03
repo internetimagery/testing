2613cdd81e6ba659cf66629436bf6fb77037d9f2
Jason Dixon <jason.dixon.email@gmail.com>
2018-03-04 00:23:51 +1300

    on to something

--- estimator.py
+++ estimator.py
@@ -1,25 +1,24 @@
 # learning estimators
 from __future__ import print_function
 import tensorflow as tf
-import numpy
 import os.path
+import shutil
 import os
 import random
 
 # https://stackoverflow.com/questions/47068709/your-cpu-supports-instructions-that-this-tensorflow-binary-was-not-compiled-to-u
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # hide warning
 
-temp = os.path.join(os.path.dirname(__file__), "temp")
+temp = os.path.join(os.path.dirname(__file__), "temp_brain")
+if os.path.exists(temp):
+    shutil.rmtree(temp)
+os.mkdir(temp)
 
 def input_fn():
     # Generate data
-    # return {"train": numpy.array/([a for a in range(100)])}, numpy.array([a+3 for a in range(100)])
-    vals = [float(random.randint(0,9)) for _ in range(200)]
+    vals = [float(random.randint(0,9)) for _ in range(500)]
     compare = [a+3 for a in vals]
     return {"train": vals}, compare
-
-def eval_fn():
-    return {"train": [5]}, [5+3]
 
 def predict_fn():
     return {"train": [5]}
@@ -30,14 +29,19 @@ def main():
     # features
     train = tf.feature_column.numeric_column("train")
 
-    estimator = tf.estimator.LinearRegressor(
+    # estimator = tf.estimator.LinearRegressor(
+    #     feature_columns=[train],
+    #     model_dir=temp)
+
+    estimator = tf.estimator.DNNRegressor(
         feature_columns=[train],
+        hidden_units=[1024,512,256],
         model_dir=temp)
 
     estimator.train(input_fn=input_fn, steps=1000)
     # print({a: estimator.get_variable_value(a) for a in estimator.get_variable_names()})
     print("evaluate", estimator.evaluate(input_fn=input_fn, steps=100))
-    print("predict", estimator.predict(input_fn=predict_fn).__next__())
+    print("expected 3, got", estimator.predict(input_fn=predict_fn).__next__()["predictions"][0])
 
 
 if __name__ == '__main__':
